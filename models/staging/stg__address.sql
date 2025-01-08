1b2b0e9b1dd48c6286ade5452da9853dd0ba9108
mattheus  nascimento <mmsnascimento23@gmail.com>
2025-01-08 20:00:16 +0000

    number of orders test

--- models/staging/stg__address.sql
+++ models/staging/stg__address.sql
@@ -8,12 +8,12 @@ with
 
 , new_address as (
         select
-        addressid as address_id
-        , stateprovinceid as stateprovince_id
-        , coalesce(addressline1, '') || ' ' || coalesce(addressline2, '') AS full_address
-        , city
-        , postalcode
-        , spatiallocation
+            addressid as address_id
+            , stateprovinceid as stateprovince_id
+            , coalesce(addressline1, '') || ' ' || coalesce(addressline2, '') AS full_address
+            , city
+            , postalcode
+            , spatiallocation
         from address 
 )
 
